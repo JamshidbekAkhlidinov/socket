133268d8eca65bf8e02d820eed6d5b904f4304f9
Jamshidbek Axlidinov <jamshidbekaxlidinovtatu@gmail.com>
2022-05-07 11:01:37 +0500

    Update chatroom.php

--- chatroom.php
+++ chatroom.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+if(isset($_SESSION['user'])){
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,7 +17,7 @@ session_start();
 <body>
     <h1 align='center'>Chatga hush kelibsiz</h1>
     <hr>
-    <div class="container row">
+    <div class="container-fluid row">
         <div class="col-lg-4">
             <table class="table table-striped">
                 <tr>
@@ -24,8 +25,10 @@ session_start();
                 </tr>
                 <tr>
                     <td>
+                        
                         <?php
                     foreach($_SESSION['user'] as $key=>$value){
+                        echo '<input type="hidden" name="userId" id="userId" value="'.$key.'">';
                         echo "<div>".$value['name']."</div>";
                         echo "<div>".$value['email']."</div>";
                     }
@@ -63,13 +66,63 @@ session_start();
                             </th>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td valign="top">
+                                <div><b>Form</b></div>
+                                <div>Message</div>
+                            </td>
+                            <td valign="top" align="right">Message Time</td>
                         </tr>
                     </table>
                 </div>
+                <form action="" method="" class="chat-room-frm">
+                    <div class="form-group">
+                        <textarea name="msg" id="msg" cols="30" rows="10" class="form-control" placeholder="Enter message"></textarea>
+                    </div>
+                    <div class="form-group col-lg-12">
+                        <input type="button" value="Send" class="btn btn-success" id="send" name="send">
+                    </div>
+                </form>
         </div>
     </div>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+       
+       $(document).ready(function(){
+        var conn = new WebSocket('ws://localhost:8080');
+        conn.onopen = function(e) {
+            console.log("Connection established!");
+        };
+
+        conn.onmessage = function(e) {
+            console.log(e.data);
+        };
+
+
+        $("#send").click(function(){
+            var userId = $("#userId").val();
+            var msg = $("#msg").val();
+            $("#msg").val('');
+
+            var data = {
+                userId: userId,
+                msg: msg
+            };
+            conn.send(JSON.stringify(data));
+        });
+
+       });
+
+
+    </script>
 
 </body>
 
 </html>
+
+<?php
+}else{
+    header('location: login.php');
+}
+?>
